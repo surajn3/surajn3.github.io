df7ec49bcf6c7447a9930288ffc7e41064cc7635
Suraj Nayak <surajn3@illinois.edu>
2020-08-01 02:39:51 -0700

    added simple bar chart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 async function init() {
     var visualizationTarget = d3.select("#visualizationTarget");
-    console.log(visualizationTarget);
+    //console.log(visualizationTarget.node().getBoundingClientRect().width);
+
+    var width = visualizationTarget.node().getBoundingClientRect().width;
+    var height = 400;
 
     var data = ["Hello"];
 
@@ -12,7 +15,52 @@ async function init() {
     console.log(data.length);
     
     // Filter data based on selected location
-    console.log(data.filter(function(d){
+    var countryData = data.filter(function(d){
        return d.location === "United States";
-    }).length);
+    })
+
+    var maxNewCases = d3.max(countryData, function(d){
+                    return parseInt(d.new_cases);
+                });
+
+    console.log("maxNewCases : " + maxNewCases)
+
+    var y = d3.scaleLinear()
+                .domain([0, maxNewCases])
+                .range([height, 0]);
+
+    var x = d3.scaleLinear()
+                .domain([0, countryData.length])
+                .range([0, width]);            
+
+    console.log("y(0) : " +y(0));
+    console.log("y(3000) : " +y(78427));
+
+    visualizationTarget
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .classed("barchart", true);
+
+
+    var svg = d3.select(".barchart");
+
+    svg.selectAll("rect")
+        .data(countryData)
+        .enter()
+            .append("rect")
+            .attr("width", x(1) - 1)
+            .attr("height", function(d) {
+                //console.log(y(parseInt(d.new_cases)));
+                return height - y(parseInt(d.new_cases));
+            })
+            .attr("x", function(d, i){
+                return x(i);
+            })
+            .attr("y", function(d){
+                return y(parseInt(d.new_cases));
+            });
+
+
+        
 }
