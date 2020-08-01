706a2a8981e4e701e1d86fd73cee1fb43b5f5160
Suraj Nayak <surajn3@illinois.edu>
2020-07-31 22:21:52 -0700

    Created Page Template

---
+++ js/script.js
@@ -0,0 +1,13 @@
+async function init() {
+    var visualizationTarget = d3.select("#visualizationTarget");
+    console.log(visualizationTarget);
+
+    var data = ["Hello"];
+
+    visualizationTarget
+        .append("h1")
+        .text("Hello")
+
+    // var data = await d3.csv("https://raw.githubusercontent.com/surajn3/surajn3.github.io/master/data/owid-covid-data.csv");
+    // console.log(data.length);
+}
