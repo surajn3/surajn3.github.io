9ea4b647b99bcf25f5b73f738c8681889854a8c2
Suraj Nayak <surajn3@illinois.edu>
2020-08-02 16:07:43 -0700

    added highlight on mouse over for circles

--- js/script.js
+++ js/script.js
@@ -85,12 +85,7 @@ async function firstPage() {
 
     //d.setDate(d.getDate() + 1)
     var startDate = dateParser(countryData[0].date);
-    console.log(startDate);
     startDate.setDate(startDate.getDate() + 1);
-    console.log(startDate);
-    console.log(timeScale(startDate));
-    console.log(dateParser(countryData[1].date) + " " + timeScale(dateParser(countryData[1].date)));
-    //console.log((dateParser(countryData[0].date) + 1) + " " + timeScale(dateParser(countryData[0].date)) + 1);
     
     svg.append("g")
         .selectAll("rect")
@@ -149,25 +144,22 @@ async function secondPage() {
     
     // Filter data based on selected date
     var countryData = data.filter(function(d){
-        return d.date === "2020-06-01" & d.location != "World";})
+        return d.date === "2020-07-28" & d.location != "World";})
 
     var yData = countryData.map(function(d){return parseFloat(d['total_cases']);});
 
     var xData = countryData.map(function(d){return parseFloat(d['total_deaths']);});
 
-    console.log("Extent x data : " + d3.extent(xData));
-    console.log("Extent y data : " + d3.extent(yData));
-
     // Add 5% extra room on Y axis for clarity
     var y = d3.scaleLog()
                 .base(10)
-                .domain([1,2000000])
+                .domain([1,8000000])
                 .range([height, 0]);
 
     var x = d3.scaleLog()
                 .base(10)
-                .domain([1,120000])
-                .range([0, width - 40]);                    
+                .domain([1,300000])
+                .range([0, width - 45]);                    
 
     //countryDropDownDiv
     var locations = countryData.map(function(d){return d.location;}).sort();
@@ -182,24 +174,50 @@ async function secondPage() {
     
 
     svg.append("g")
-        .attr("transform","translate(20,0)")
+        .attr("transform","translate(45,10)")
         .selectAll("circle")
         .data(countryData)
         .enter()
             .append("circle")
+            .classed("default-circle-color", true)
+            .on("mouseover", function(d,i){
+                d3.select(this)
+                    .classed("default-circle-color",false)
+                    .classed("highlighted-circle-color",true)
+                    .attr("r", 4 + 3);
+
+                console.log("Mouse Over on " + d.location);
+            })
+            .on("mouseout", function(d,i){
+                d3.select(this)
+                    .classed("highlighted-circle-color",false)
+                    .classed("default-circle-color",true)
+                    .attr("r", 4);
+
+                console.log("Mouse Out on " + d.location);
+            })
             .attr("cx", function(d){
                 var scaledX = x(parseFloat(d['total_deaths']));
-                console.log("Calculated scaledX : " + scaledX)
                 if(isNaN(scaledX)) {
                     scaledX = 0;
                 }
-                console.log("Nan Replaced scaledX : " + scaledX)
                 return scaledX;
             })
+            .attr("cy",height)
+            .attr("r", 4)
+            .transition()
+            .duration(2000)
             .attr("cy", function(d){return y(parseFloat(d['total_cases']));})
-            .attr("r", 5)
-                .append("title")
-                .text(function(d){ return d.location;})
+
+
+    svg.append("g")
+        .attr("transform", "translate(40,-30)")
+        .call(d3.axisLeft(y).tickFormat(d3.format("~s")).tickValues([1, 100, 500, 10000, 100000, 500000, 1000000, 1500000, 2000000, 2500000]));
+
+
+    svg.append("g")
+        .attr("transform", "translate(45," + (height - 30) +")")
+        .call(d3.axisBottom(x).tickFormat(d3.format("~s")).tickValues([1, 100, 500, 10000, 50000, 100000, 200000, 500000, 1000000, 2000000]));             
 
          
 }
@@ -208,42 +226,60 @@ async function secondPage() {
 async function thirdPage() {
     var countryName = "United States";
 
+    // Cleanup
+    d3.select("svg").remove();
+    d3.select("#dropDownButton").remove();
+    d3.select("#countryDropDownDiv").remove();
+
+
     var visualizationTarget = d3.select("#visualizationTarget");
-    var countryDropDownDiv = d3.select("#countryDropDownDiv");
 
     var width = visualizationTarget.node().getBoundingClientRect().width;
-    var height = 400;
+    var height = 800;
 
     if(data.length == 0) {
         data = await d3.csv("https://raw.githubusercontent.com/surajn3/surajn3.github.io/master/data/owid-covid-data.csv");
         console.log("Loaded "+ data.length + " records.");
     }
-    
-    // Filter data based on selected location
-    var countryData = data.filter(function(d){
-       return d.location === countryName;
+
+    data = data.filter(function(d){
+        return d.date > "2020-04-15" & d.location != "World";
     });
 
-    var countryTotal = countryData.map(function(d){ return d.total_cases;});
-    console.log("countryTotal : " + countryTotal);
-
-    //countryDropDownDiv
     var locations = Array.from(new Set(data.map(function(d){return d.location;}))).sort();
 
+    var locations = ["United States", "Brazil", "Italy", "India"];
+    
+    // Filter data based on selected location
+    var countryData = locations.map(function(loc){
+        return data.filter(function(d){
+        return d.location === loc;
+    });
+    });
+    
     var dateParser = d3.timeParse("%Y-%m-%d");
 
-    var maxTotalCases = d3.max(countryData, function(d){
+    // TODO : Change this to include data only for top N countries
+    var maxTotalCases = d3.max(data, function(d){
                     return parseFloat(d.total_cases);
                 });
 
 
     // Add 5% extra room on Y axis for clarity
+    /*
+    var y = d3.scaleLog()
+                .base(2)
+                .domain([1, maxTotalCases + maxTotalCases * 0.05])
+                .range([height, 0]);
+    */
+
+
     var y = d3.scaleLinear()
                 .domain([0, maxTotalCases + maxTotalCases * 0.05])
-                .range([height, 0]);
+                .range([height, 0]);            
 
     var timeScale = d3.scaleTime()
-                        .domain(d3.extent(countryData, function(d){
+                        .domain(d3.extent(data, function(d){
                             return dateParser(d.date);
                         }))
                         .range([0, width - 40]);
@@ -254,7 +290,7 @@ async function thirdPage() {
                     return timeScale(date);
                 })
                 .y(function(d){
-                    return y(d.total_cases);
+                    return y(parseFloat(d.total_cases) + 0.2);
                 });
 
      visualizationTarget
@@ -267,11 +303,20 @@ async function thirdPage() {
     var svg = d3.select(".trendline");
 
     svg.append("g")
-        .attr("transform", "translate(0,-10)")
+        .attr("transform", "translate(80,-30)")
         .selectAll("path")
-        .data([countryData])
+        .data(countryData)
         .enter()
             .append("path")
             .attr("d", function(d){ return line(d);})
+
+    svg.append("g")
+        .attr("transform", "translate(80,-30)")
+        .call(d3.axisLeft(y));
+
+
+    svg.append("g")
+        .attr("transform", "translate(80," + (height - 30) +")")
+        .call(d3.axisBottom(timeScale));        
 
 }
